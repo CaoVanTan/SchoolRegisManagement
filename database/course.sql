3c8804394d55d37035ec10386d86772d4c37fc10
chikien2412 <chikien2412@gmail.com>
2021-10-26 22:52:06 +0700

    add data

--- database/course.sql
+++ database/course.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: localhost
--- Thời gian đã tạo: Th10 26, 2021 lúc 02:42 PM
--- Phiên bản máy phục vụ: 8.0.17
--- Phiên bản PHP: 7.3.10
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 26, 2021 lúc 05:46 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `db_dkhsv`
+-- Cơ sở dữ liệu: `dkhsv`
 --
 
 -- --------------------------------------------------------
@@ -33,12 +32,12 @@ CREATE TABLE `course` (
   `course_name` varchar(50) NOT NULL,
   `course_teacher` varchar(50) NOT NULL,
   `credits` int(11) NOT NULL,
-  `tuition` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tuition` varchar(50) DEFAULT NULL,
   `time_start` date NOT NULL,
   `time_end` date NOT NULL,
   `status` varchar(50) NOT NULL DEFAULT 'Bình thường',
   `numbers` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `course`
@@ -46,16 +45,6 @@ CREATE TABLE `course` (
 
 INSERT INTO `course` (`course_id`, `course_name`, `course_teacher`, `credits`, `tuition`, `time_start`, `time_end`, `status`, `numbers`) VALUES
 ('1', 'Công nghệ web', 'Kiều Tuấn Dũng', 3, NULL, '2021-09-06', '2021-10-31', 'Bình thường', 70);
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `course`
---
-ALTER TABLE `course`
-  ADD PRIMARY KEY (`course_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- database/office.sql
+++ database/office.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: localhost
--- Thời gian đã tạo: Th10 26, 2021 lúc 02:42 PM
--- Phiên bản máy phục vụ: 8.0.17
--- Phiên bản PHP: 7.3.10
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 26, 2021 lúc 05:46 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `db_dkhsv`
+-- Cơ sở dữ liệu: `dkhsv`
 --
 
 -- --------------------------------------------------------
@@ -31,35 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `office` (
   `office_id` int(10) UNSIGNED NOT NULL,
   `office_name` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Đang đổ dữ liệu cho bảng `office`
---
-
-INSERT INTO `office` (`office_id`, `office_name`) VALUES
-(1, 'Công nghệ thông tin'),
-(2, 'Kĩ thuật phần mềm');
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `office`
---
-ALTER TABLE `office`
-  ADD PRIMARY KEY (`office_id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `office`
---
-ALTER TABLE `office`
-  MODIFY `office_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
