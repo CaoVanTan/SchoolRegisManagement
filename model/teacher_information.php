f6379359e4d4e660476c5422ed34332eb500088f
chikien2412 <chikien2412@gmail.com>
2021-10-30 18:30:49 +0700

    update code model

--- model/teacher_information.php
+++ model/teacher_information.php
@@ -6,12 +6,19 @@
      $sql = "SELECT t.teacher_id,t.teacher_name,t.teacher_gender,t.teacher_birthday,t.teacher_address,t.teacher_phone,t.teacher_email,t.teacher_pass FROM teacher t ";
      $result = mysqli_query($con,$sql); //Lưu kết quả trả về vào result
      //Bước 03: Phân tích và xử lý kết quả
+     $gender;
      if(mysqli_num_rows($result)>0){
          while($row=mysqli_fetch_assoc($result)){
              echo '<tr>';
              echo '<th scope="row">'.$row['teacher_id'].'</th>';
              echo '<td>'.$row['teacher_name'].'</td>';
-             echo '<td>'.$row['teacher_gender'].'</td>';
+             
+             if($row['teacher_gender']){
+                 $gender='Nam';
+             }else{
+                 $gender='Nữ';
+             }
+             echo '<td>'.$gender.'</td>';
              echo '<td>'.$row['teacher_birthday'].'</td>';
              echo '<td>'.$row['teacher_address'].'</td>';
              echo '<td>'.$row['teacher_phone'].'</td>';
