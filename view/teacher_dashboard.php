e8588ca9e1e9a3b4816a651c31ce2c79c509cbe0
Cao Van Tan <minhtan006237@gmail.com>
2021-11-02 21:16:34 +0700

    update(error)

---
+++ view/teacher_dashboard.php
@@ -0,0 +1,39 @@
+<?php
+include '../partials-front/header.php';
+include '../partials-front/header_nav.php';
+include '../partials-front/teacher_menu.php';
+?>
+
+<main class="d-flex justify-content-end">
+    <div class="col-10">
+        <div class="container">
+            <div class="row justify-content-between">
+        
+                <a href="../view/subject_information.php"
+                    class="manage_item col-5 btn btn-primary my-4 mx-3 d-flex justify-content-center align-items-center ">
+
+
+                    <span class="icon"> Thông tin Khóa học </span>
+
+
+
+
+
+                </a>
+                <a href="../view/student_register.php"
+                    class="manage_item col-5 btn btn-primary my-4 mx-3 d-flex justify-content-center align-items-center">
+                     Sinh viên đăng kí 
+                </a>
+                <a href="../view/teacher_information.php"
+                    class="manage_item col-5 btn btn-primary my-4 mx-3 d-flex justify-content-center align-items-center">
+                     Thông tin cá nhân 
+                </a>
+
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+include '../partials-front/footer.php';
+?>
---
+++ view/teacher_information.php
@@ -0,0 +1,43 @@
+<?php
+include '../partials-front/header.php';
+include '../partials-front/header_nav.php';
+include '../partials-front/teacher_menu.php';
+?>
+
+<main class="d-flex justify-content-end">
+    <div class="col-10">
+        <div class="container">
+            <div class="row justify-content-between">
+
+                <h1 class="text-center">Thông tin Giảng viên</h1>
+                
+
+                <table class="table mt-5 border border-1 table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Mã giảng viên</th>
+                            <th scope="col">Tên giảng viên</th>
+                            <th scope="col">Giới tính</th>
+                            <th scope="col">Ngày sinh</th>
+                            <th scope="col">Địa chỉ</th>
+                            <th scope="col">Số điện thoại</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Mật khẩu</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                  
+                    </tbody>
+                </table>
+
+
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php
+include '../partials-front/footer.php';
+?>
