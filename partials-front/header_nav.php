952079da2f2d306a9a1e7b8be3d9c8b6b8605b0d
NguyenManh <manh95405@gmail.com>
2021-11-03 09:31:28 +0700

    Update code

---
+++ partials-front/header_nav.php
@@ -0,0 +1,21 @@
+<header class="header ">
+    <div class="container h-100 d-flex justify-content-between align-items-center">
+        <a href="#" class="logo">
+            <img class="logo-img" src="http://tlus.edu.vn/wp-content/uploads/2018/06/LogoDHTL-300x300.png" alt="Logo">
+        </a>
+
+        <div class="dropdown h-100 menu">
+            <button class="btn dropdown-toggle h-100 text-white" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Admin
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li>
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-power-off me-2"></i>
+                        Đăng xuất
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
