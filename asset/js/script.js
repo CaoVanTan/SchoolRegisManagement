1f07bf8f539d30c78f2dcc98fe3d0f41600849b5
Cao Van Tan <minhtan006237@gmail.com>
2021-11-03 12:31:49 +0700

    update css and js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,37 +1,14 @@
 $(document).ready(function () {
-  // Add subject
-  $("#btnAddSubject").click(function () {
-    $("#modelAddSubject").show();
-    $("#overlayAddSubject").show();
+
+  // Model add
+  $(".btnAdd").click(function () {
+    $(".overlay").show();
+    $(".model").show();
   });
 
-  $("#btnCancelAddSubject").click(function () {
-    $("#overlayAddSubject").hide();
-    $("#modelAddSubject").hide();
-  });
-
-
-  // Add teacher
-  $("#btnAddTeacher").click(function () {
-    $("#modelAddTeacher").show();
-    $("#overlayAddTeacher").show();
-  });
-
-  $("#btnCancelAddTeacher").click(function () {
-    $("#overlayAddTeacher").hide();
-    $("#modelAddTeacher").hide();
-  });
-
-
-  // Add student
-  $("#btnAddStudent").click(function () {
-    $("#modelAddStd").show();
-    $("#overlayAddStd").show();
-  });
-
-  $("#btnCancelAddStd").click(function () {
-    $("#overlayAddStd").hide();
-    $("#modelAddStd").hide();
+  $(".btnCancelAdd").click(function () {
+    $(".overlay").hide();
+    $(".model").hide();
   });
 
 
