ce6cf2ede16777c93baa137ed55bcfd2cfe225bd
Cao Van Tan <minhtan006237@gmail.com>
2021-11-03 14:01:08 +0700

    delete all file

--- process/admin/get_curriculum.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../../config/config.php';
-
-    $sql = "SELECT cur.*, sub.subject_name, off.office_name, c.course_id FROM curriculum cur, subject sub, office off, course c
-            WHERE cur.office_id = off.office_id AND cur.subject_id = sub.subject_id AND cur.course_id = c.course_id";
-    $result = mysqli_query($con, $sql);
-?>
--- process/admin/get_list_register.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../../config/config.php';
-
-    $sql = "SELECT list.*, std.*, sub.*, tc.*, c.*  FROM list_register list, student std, subject sub, teacher tc, class c
-            WHERE list.std_id = std.std_id AND list.subject_id = sub.subject_id AND list.teacher_id = tc.teacher_id AND std.class_id = c.class_id";
-    $result = mysqli_query($con, $sql);
-?>
